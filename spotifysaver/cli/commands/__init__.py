b56063ce00419ded93df99d1ab64324436594101
gabrielbaute <gabrielbaute@gmail.com>
2025-06-04 15:59:40 -0400

    Lista de comandos

---
+++ spotifysaver/cli/commands/__init__.py
@@ -0,0 +1,9 @@
+from spotifysaver.cli.commands.download import download
+from spotifysaver.cli.commands.version import version
+from spotifysaver.cli.commands.inspect import inspect
+
+__all__ = [
+    "download",
+    "version",
+    "inspect",
+    ]
