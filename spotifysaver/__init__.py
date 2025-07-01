034cb4055288d635f872ab8466f970a2f364319e
gabrielbaute <gabrielbaute@gmail.com>
2025-07-01 19:00:23 -0400

    bump version 0.5.0 -> 0.5.1

--- spotifysaver/__init__.py
+++ spotifysaver/__init__.py
@@ -19,7 +19,7 @@ to source the actual audio files, ensuring high-quality downloads with complete
 metadata preservation.
 """
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 
 # Verify if ffmpeg is installed
