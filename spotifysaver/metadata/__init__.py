1d84fa552b0d100bd5edf9374de96f49875f2c28
gabrielbaute <gabrielbaute@gmail.com>
2025-11-19 10:17:41 -0400

    Service to fetch the audiodb metadata

--- spotifysaver/metadata/__init__.py
+++ spotifysaver/metadata/__init__.py
@@ -2,8 +2,10 @@
 
 from spotifysaver.metadata.nfo_generator import NFOGenerator
 from spotifysaver.metadata.music_file_metadata import MusicFileMetadata
+from spotifysaver.metadata.the_audio_db_service import TheAudioDBService
 
 __all__ = [
     "NFOGenerator",
-    "MusicFileMetadata"
+    "MusicFileMetadata",
+    "TheAudioDBService"
 ]
