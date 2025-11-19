4cb84f37d52632d11d9385468300ae283f3fbf59
gabrielbaute <gabrielbaute@gmail.com>
2025-11-19 15:07:15 -0400

    The audiodb service moved to services module

--- spotifysaver/metadata/__init__.py
+++ spotifysaver/metadata/__init__.py
@@ -2,10 +2,8 @@
 
 from spotifysaver.metadata.nfo_generator import NFOGenerator
 from spotifysaver.metadata.music_file_metadata import MusicFileMetadata
-from spotifysaver.metadata.the_audio_db_service import TheAudioDBService
 
 __all__ = [
     "NFOGenerator",
     "MusicFileMetadata",
-    "TheAudioDBService"
 ]
