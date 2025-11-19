8ecb0d8ff0ba74ebafc3427b60908da48bab5ec3
gabrielbaute <gabrielbaute@gmail.com>
2025-11-19 15:06:56 -0400

    The audiodb service moved to services module

---
+++ spotifysaver/services/the_audio_db_service.py
@@ -0,0 +1,165 @@
+import requests
+from typing import Optional, Dict, Any, List
+
+from spotifysaver.spotlog import get_logger
+from spotifysaver.services.schemas import (
+    TrackADBResponse,
+    AlbumADBResponse,
+    MediaAlbumURLs,
+    AlbumDescription,
+    MediaArtistURLs,
+    ArtistBiography,
+    ArtistFanart,
+    ArtistADBResponse
+    )
+
+class TheAudioDBService():
+    """
+    Search for metadata in TheAudioDB.
+
+    This class provides methods to obtain metadata for artist, albums and tracks from TheAudioDB.
+    """
+    def __init__(self):
+        self.logger = get_logger(f"{__class__.__name__}")
+        self.url_base = "https://www.theaudiodb.com/api/v1/json/123/"
+
+    def _search_artist_by_name(self, artist_name: str) -> Optional[Dict[str, Any]]:
+        """
+        Search for an artist by name.
+
+        Args:
+            artist_name (str): The name of the artist to search for.
+
+        Returns:
+            Optional[Dict[str, Any]]: A dictionary containing the artist information if found, otherwise None.
+        """
+        self.logger.info(f"Searching artist by name: {artist_name}")
+        try:
+            response = requests.get(f"{self.url_base}search.php?s={artist_name}")
+            if response.status_code == 200:
+                raw = response.json()
+                return raw.get("artists", [{}])[0]
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error: {e}")
+            return None
+    
+    def _search_album_by_name(self, artist_name: str, album_name: str) -> Optional[Dict[str, Any]]:
+        """
+        Search for an album by name and artist.
+
+        Args:
+            artist_name (str): The name of the artist.
+            album_name (str): The name of the album.
+        
+        Returns:
+            Optional[Dict[str, Any]]: A dictionary containing the album information if found, otherwise None.
+        """
+        self.logger.info(f"Searching album by name: {album_name}")
+        try:
+            response = requests.get(f"{self.url_base}searchalbum.php?s={artist_name}&a={album_name}")
+            if response.status_code == 200:
+                raw = response.json()
+                return raw.get("album", [{}])[0]
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error: {e}")
+            return None
+    
+    def _search_track_by_name(self, artist_name: str, track_name: str) -> Optional[Dict[str, Any]]:
+        """
+        Search for a track by name and artist.
+
+        Args:
+            artist_name (str): The name of the artist.
+            track_name (str): The name of the track.
+        
+        Returns:
+            Optional[Dict[str, Any]]: A dictionary containing the track information if found, otherwise None.
+        """
+        self.logger.info(f"Searching track by name: {track_name}")
+        try:
+            response = requests.get(f"{self.url_base}searchtrack.php?s={artist_name}&t={track_name}")
+            if response.status_code == 200:
+                raw = response.json()
+                return raw.get("track", [{}])[0]
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error: {e}")
+            return None
+
+    def _get_tracks_from_an_album(self, album_id: str) -> Optional[List[Dict[str, Any]]]:
+        """
+        Get tracks from an album.
+
+        Args:
+            album_id (str): The ID of the album.
+        
+        Returns:
+            Optional[List[Dict[str, Any]]]: A dictionary containing the tracks information if found, otherwise None.
+        """
+        self.logger.info(f"Getting tracks from album: {album_id}")
+        try:
+            response = requests.get(f"{self.url_base}track.php?m={album_id}")
+            if response.status_code == 200:
+                raw = response.json()
+                return raw.get("track", [{}])
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error: {e}")
+            return None
+
+    def _search_track_by_id(self, track_id: str) -> Optional[Dict[str, Any]]:
+        """
+        Search for a track by ID.
+
+        Args:
+            track_id (str): The ID of the track.
+        
+        Returns:
+            Optional[Dict[str, Any]]: A dictionary containing the track information if found, otherwise None.
+        """
+        self.logger.info(f"Searching track by ID: {track_id}")
+        try:
+            response = requests.get(f"{self.url_base}track.php?i={track_id}")
+            if response.status_code == 200:
+                raw = response.json()
+                return raw.get("track", [{}])[0]
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error: {e}")
+            return None
+        
+    def get_track_metadata(
+            self, 
+            track_name: str, 
+            artist_name: Optional[str] = None, 
+            ) -> TrackADBResponse:
+        """
+        Get the metadata of a track.
+
+        Args:
+            track_name (str): The name of the track.
+            artist_name (Optional[str]): The name of the artist.
+            album_name (Optional[str]): The name of the album.
+        
+        Returns:
+            TrackADBResponse: A dictionary containing the track metadata.
+        """
+        raw_data = self._search_track_by_name(artist_name, track_name)
+        if not raw_data:
+            return None
+        
+        data = TrackADBResponse(
+            id=int(raw_data.get("idTrack", None)),
+            name=raw_data.get("strTrack", None),
+            album_id=int(raw_data.get("idAlbum", None)),
+            album_name=raw_data.get("strAlbum", None),
+            artist_id=[int(raw_data.get("idArtist", None))],
+            artist_name=[raw_data.get("strArtist", None)],
+            duration=int(raw_data.get("intDuration", None)),
+            track_number=int(raw_data.get("intTrackNumber", None)),
+            genre=raw_data.get("strGenre", None),
+            mood=raw_data.get("strMood", None),
+            style=raw_data.get("strStyle", None),
+            lyrics=raw_data.get("strLyrics", None),
+            musicbrainz_id=raw_data.get("strMusicBrainzID", None),
+            album_musicbrainz_id=raw_data.get("strMusicBrainzAlbumID", None),
+            artist_musicbrainz_id=raw_data.get("strMusicBrainzArtistID", None)
+        )
+        return data
