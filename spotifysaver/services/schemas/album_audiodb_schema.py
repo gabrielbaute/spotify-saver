cc51d54a940791c6b7e3d6ee61ffe6471ca8d200
gabrielbaute <gabrielbaute@gmail.com>
2025-11-19 16:56:25 -0400

    Docstrings added. Fanart schemas deleted

--- spotifysaver/services/schemas/album_audiodb_schema.py
+++ spotifysaver/services/schemas/album_audiodb_schema.py
@@ -1,11 +1,30 @@
 from pydantic import BaseModel
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 class AlbumDescription(BaseModel):
+    """
+    Response Schema for Album metadata from TheAudioDB.
+
+    keywords:
+        language (str): Language of the description
+        description (str): Description of the album
+    """
     language: str
     description: str
 
 class MediaAlbumURLs(BaseModel):
+    """
+    Response Schema for Album URLs from TheAudioDB.
+
+    keywords:
+        thumb_url (Optional[str]): URL of the album thumbnail
+        back_url (Optional[str]): URL of the album back cover
+        cd_art_url (Optional[str]): URL of the album CD art
+        case_url (Optional[str]): URL of the album 3D case
+        face_url (Optional[str]): URL of the album face
+        flat_url (Optional[str]): URL of the album 3D flat
+        cd_thumb_url (Optional[str]): URL of the album 3D thumbnail
+    """
     thumb_url: Optional[str]
     back_url: Optional[str]
     cd_art_url: Optional[str]
@@ -15,6 +34,24 @@ class MediaAlbumURLs(BaseModel):
     cd_thumb_url: Optional[str]
 
 class AlbumADBResponse(BaseModel):
+    """
+    Response Schema for Album metadata from TheAudioDB.
+
+    keywords:
+        id (int): ID of the album in TheAudioDB
+        name (str): Name of the album
+        artist_id (int): ID of the artist in TheAudioDB
+        artist_name (str): Name of the artist
+        genre (Optional[str]): Genre of the album
+        style (Optional[str]): Style of the album
+        mood (Optional[str]): Mood of the album
+        musicbrainz_id (Optional[str]): MusicBrainz ID of the album
+        artist_musicbrainz_id (Optional[str]): MusicBrainz ID of the artist
+        release_format (Optional[str]): Release format of the album
+        release_date (Optional[str]): Release date of the album
+        description (Optional[List[AlbumDescription]]): List of descriptions of the album
+        media_album (Optional[MediaAlbumURLs]): URLs of the album media
+    """
     id: int
     name: str
     artist_id: int
--- spotifysaver/services/schemas/track_audiodb_schema.py
+++ spotifysaver/services/schemas/track_audiodb_schema.py
@@ -2,6 +2,26 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class TrackADBResponse(BaseModel):
+    """
+    Response Schema for Track metadata from TheAudioDB.
+
+    keywords:
+        id (int): ID of the track in TheAudioDB
+        name (str): Name of the track
+        album_id (int): ID of the album containing the track in TheAudioDB
+        album_name (str): Name of the album containing the track
+        artist_id (List[int]): List of IDs of the artists in TheAudioDB
+        artist_name (List[str]): List of names of the artists
+        duration (int): Duration of the track in seconds
+        track_number (int): Track number in the album
+        genre (Optional[str]): Genre of the track
+        mood (Optional[str]): Mood of the track
+        style (Optional[str]): Style of the track
+        lyrics (Optional[str]): Lyrics of the track
+        musicbrainz_id (Optional[str]): MusicBrainz ID of the track
+        album_musicbrainz_id (Optional[str]): MusicBrainz ID of the album
+        artist_musicbrainz_id (Optional[str]): MusicBrainz ID of the artist
+    """
     id: int
     name: str
     album_id: int
